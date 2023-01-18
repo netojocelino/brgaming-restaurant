6577401db66070224fb02aa3db8dda92f4ae0bc3
Neto Jocelino <netoj96@live.com>
2023-01-18 18:57:37 -0300

    restaurantRepository returns items added

--- tests/list-restaurants-repository.spec.ts
+++ tests/list-restaurants-repository.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from "vitest";
 
-import Restaurant from "./entity/Restaurant";
+import Restaurant from "../src/entity/Restaurant";
 
 interface IRestaurantRepository<T> {
     save(item: T): Promise<T>
@@ -9,14 +9,14 @@ interface IRestaurantRepository<T> {
 
 
 class RestaurantRepository implements IRestaurantRepository<Restaurant> {
-    private items: Restaurant[] = []
+    items: Restaurant[] = []
 
     async save (item: Restaurant) : Promise<Restaurant> {
         throw new Error('Not implemented yet')
     }
 
     async listAll(): Promise<Restaurant[]> {
-        return []
+        return this.items
     }
 }
 
@@ -26,5 +26,22 @@ test('Should list empty array when not exists restaurant saved', async () => {
     const sut = await repository.listAll()
 
     expect(sut.length).toBe(0)
+    expect(sut).instanceOf(Array)
+})
+
+
+test('Should list one restaurant when exists on item ', async () => {
+    const repository = new RestaurantRepository()
+    const restaurant = {
+        name: 'Papa\'s Jocelino Restaurant',
+        document_id: '1234778-88',
+        type: 'SnackBar',
+    }
+
+    repository.items.push(new Restaurant(restaurant))
+
+    const sut = await repository.listAll()
+
+    expect(sut.length).toBe(1)
     expect(sut).instanceOf(Array)
 })
