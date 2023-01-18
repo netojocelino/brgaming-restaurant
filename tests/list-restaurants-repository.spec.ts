a10a3d2a8a9a6cd9fa189d4b7b883b5acb378337
Neto Jocelino <netoj96@live.com>
2023-01-18 18:48:30 -0300

    RestaurantRepository must return empty when hasn't items saved

---
+++ tests/list-restaurants-repository.spec.ts
@@ -0,0 +1,30 @@
+import { expect, test } from "vitest";
+
+import Restaurant from "./entity/Restaurant";
+
+interface IRestaurantRepository<T> {
+    save(item: T): Promise<T>
+    listAll(): Promise<T[]>
+}
+
+
+class RestaurantRepository implements IRestaurantRepository<Restaurant> {
+    private items: Restaurant[] = []
+
+    async save (item: Restaurant) : Promise<Restaurant> {
+        throw new Error('Not implemented yet')
+    }
+
+    async listAll(): Promise<Restaurant[]> {
+        return []
+    }
+}
+
+const repository = new RestaurantRepository()
+
+test('Should list empty array when not exists restaurant saved', async () => {
+    const sut = await repository.listAll()
+
+    expect(sut.length).toBe(0)
+    expect(sut).instanceOf(Array)
+})
