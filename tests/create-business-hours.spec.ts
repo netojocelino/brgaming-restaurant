120e1f264e761460d04603ba34d984d1050e6eb7
Neto Jocelino <netoj96@live.com>
2023-01-18 20:11:04 -0300

    use manager to check if dayOfWeek is valid
    remove `id` property

--- tests/create-business-hours.spec.ts
+++ tests/create-business-hours.spec.ts
@@ -3,9 +3,27 @@ import { expect, test } from "vitest";
 type ShortWeekDayName = 'mon' | 'tue' | 'wed' | 'thu' | 'fri' | 'sat' | 'sun'
 type LongWeekDayName = 'monday' | 'tuesday' | 'wednesday' | 'thursday' | 'friday' | 'saturday' | 'sunday'
 
+class WeekDayNames {
+    private props: Record<ShortWeekDayName, LongWeekDayName> = {
+        'mon': 'monday',
+        'tue': 'tuesday',
+        'wed': 'wednesday',
+        'thu': 'thursday',
+        'fri': 'friday',
+        'sat': 'saturday',
+        'sun': 'sunday',
+    }
+
+    public getByShortName (shortName: ShortWeekDayName) {
+        return this.props[shortName]
+    }
+
+    public isValidShortName (name: string) : name is ShortWeekDayName {
+        return Object.getOwnPropertyNames(this.props).includes(name)
+    }
+}
 
 interface BusinessHourProps {
-    id: number
     name: string // ShortWeekDayName
     startTime: string
     endTime: string
@@ -13,27 +31,23 @@ interface BusinessHourProps {
 
 class BusinessHour {
     private props: BusinessHourProps
+    private readonly weekdayManager = new WeekDayNames()
 
     constructor (input: {
         dayOfWeek: string,
         startTime: string,
         endTime: string,
     }) {
-        this.props = {...input, name: input.dayOfWeek, id: -1 }
+
+        if (!this.weekdayManager.isValidShortName(input.dayOfWeek)) {
+            throw new Error('`dayOfWeek` have a invalid format.')
+        }
+
+        this.props = {...input, name: this.weekdayManager.getByShortName(input.dayOfWeek) }
     }
 
     get name () {
-        const weekDayName: Record<string, string> = {
-            'mon': 'monday',
-            'tue': 'tuesday',
-            'wed': 'wednesday',
-            'thu': 'thursday',
-            'fri': 'friday',
-            'sat': 'saturday',
-            'sun': 'sunday',
-
-        }
-        return weekDayName[this.props.name]
+        return this.props.name
     }
 
     get startAt () {
@@ -59,4 +73,15 @@ test('Should create one business hour successfully', async () => {
     expect(sut.name).toEqual('monday')
     expect(sut.startAt).toEqual('10:00')
     expect(sut.endsAt).toEqual('18:00')
+})
+
+test('Should fails when dayOfWeek is an invalid format', async () => {
+    const businessHour = {
+        dayOfWeek: 'seg',
+        startTime: '10:00',
+        endTime: '18:00',
+    }
+
+    expect(() => new BusinessHour(businessHour))
+        .toThrowError('`dayOfWeek` have a invalid format.')
 })
