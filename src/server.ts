c8eff5e411f5889b6d763663386197e9ff59cb71
Neto Jocelino <netoj96@live.com>
2023-01-19 17:33:13 -0300

    page for swagger docs

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 require('dotenv-safe').config()
 
 import express, { Request, Response } from 'express'
+import swagger from 'swagger-ui-express'
+
+import docs from '../docs/swagger.json'
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
+app.use('/docs', swagger.serve, swagger.setup(docs))
 
 app.get('/', (_request: Request, response: Response) => response.json({ message: 'Hello World' }))
 
